0c789854d2ee52d7a38919c0e8dfaebf42376740
Su Wai Phyoe <beatrizisme4@gmail.com>
2025-09-29 10:17:56 +0300

    updated iteration 2

--- week7-fepp-starter/frontend/src/pages/AddJobPage.jsx
+++ week7-fepp-starter/frontend/src/pages/AddJobPage.jsx
@@ -1,9 +1,51 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const AddJobPage = () => {
-  
+  const [title, setTitle] = useState("");
+  const [type, setType] = useState("Full-Time");
+  const [description, setDescription] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [contactEmail, setContactEmail] = useState("");
+  const [contactPhone, setContactPhone] = useState("");
+
+  const navigate = useNavigate();
+ 
+  const addJob = async (newJob) => {
+    try {
+      const res = await fetch("/api/jobs", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newJob),
+      });
+      if (!res.ok) {
+        throw new Error("Failed to add job");
+      }
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+    return true;
+  };
+
   const submitForm = (e) => {
     e.preventDefault();
-    console.log("submitForm called");
-   
+
+    const newJob = {
+      title,
+      type,
+      description,
+      company: {
+        name: companyName,
+        contactEmail,
+        contactPhone,
+      },
+    };
+
+    addJob(newJob);
+    return navigate("/");
   };
 
   return (
@@ -14,10 +56,11 @@ const AddJobPage = () => {
         <input
           type="text"
           required
-          value=""
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
         <label>Job type:</label>
-        <select >
+        <select value={type} onChange={(e) => setType(e.target.value)}>
           <option value="Full-Time">Full-Time</option>
           <option value="Part-Time">Part-Time</option>
           <option value="Remote">Remote</option>
@@ -27,26 +70,29 @@ const AddJobPage = () => {
         <label>Job Description:</label>
         <textarea
           required
-          value=""
-
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
         ></textarea>
         <label>Company Name:</label>
         <input
           type="text"
           required
-          value=""
+          value={companyName}
+          onChange={(e) => setCompanyName(e.target.value)}
         />
         <label>Contact Email:</label>
         <input
           type="text"
           required
-          value=""
+          value={contactEmail}
+          onChange={(e) => setContactEmail(e.target.value)}
         />
         <label>Contact Phone:</label>
         <input
           type="text"
           required
-          value=""
+          value={contactPhone}
+          onChange={(e) => setContactPhone(e.target.value)}
         />
         <button>Add Job</button>
       </form>
